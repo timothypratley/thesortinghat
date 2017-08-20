64dd4b76662b1d221a91f53610a7242a5d8df079
Timothy Pratley <timothypratley@gmail.com>
2017-08-19 22:44:22 -0700

    test that multiple records for the same person overwrite

--- test/thesortinghat/server_test.clj
+++ test/thesortinghat/server_test.clj
@@ -2,16 +2,31 @@
   (:require
     [clojure.test :refer :all]
     [thesortinghat.server :as server]
+    [clojure.string :as string]
     [ring.mock.request :as mock]
     [ring.util.io :as io]))
+
+(defn post [last-name first-name gender favorite-color birth-date]
+  (server/handler (assoc (mock/request :post "/records")
+                    :body (io/string-input-stream
+                            (string/join "," [last-name first-name gender favorite-color birth-date])))))
 
 (deftest handler-test
   (is (= 200 (:status (server/handler (mock/request :get "/" nil)))))
   (with-out-str
-    (is (= 200 (:status (server/handler (assoc (mock/request :post "/records") :body (io/string-input-stream "Potter,Harry,male,green,1/1/1990")))))))
+    (is (= 200 (:status (server/handler (assoc (mock/request :post "/records") :body (io/string-input-stream "Potter,Harry,male,green,1/1/1985")))))))
   (is (= 400 (:status (server/handler (assoc (mock/request :post "/records") :body (io/string-input-stream "bad,test,record"))))))
-  (is (= 200 (:status (server/handler (mock/request :get "/records/gender" nil)))))
-  (is (= 200 (:status (server/handler (mock/request :get "/records/birthdate" nil)))))
-  (is (= 200 (:status (server/handler (mock/request :get "/records/name" nil)))))
-  (is (= 404 (:status (server/handler (mock/request :post "/invalid-url" nil)))))
-  (is (= 404 (:status (server/handler (mock/request :get "/invalid-url" nil))))))
+  (is (= 200 (:status (server/handler (mock/request :get "/records/gender")))))
+  (is (= 200 (:status (server/handler (mock/request :get "/records/birthdate")))))
+  (is (= 200 (:status (server/handler (mock/request :get "/records/name")))))
+  (is (= 404 (:status (server/handler (mock/request :post "/invalid-url")))))
+  (is (= 404 (:status (server/handler (mock/request :get "/invalid-url"))))))
+
+(deftest stateful-test
+  (testing "Received two records about Harry, and one about Hermione..."
+    (post "Potter" "Harry" "male" "blue" "1/1/1985")
+    (post "Potter" "Harry" "male" "green" "1/1/1985")
+    (post "Granger" "Hermione" "female" "red" "3/1/1985")
+    (is (= "[{\"last-name\":\"Granger\",\"first-name\":\"Hermione\",\"date-of-birth\":\"3/1/1985\",\"gender\":\"female\",\"favorite-color\":\"red\"},{\"last-name\":\"Potter\",\"first-name\":\"Harry\",\"date-of-birth\":\"1/1/1985\",\"gender\":\"male\",\"favorite-color\":\"green\"}]"
+           (:body (server/handler (mock/request :get "/records/name"))))
+        "There is only two records (one for Harry, one for Hermione) returned in JSON in the correct order")))
